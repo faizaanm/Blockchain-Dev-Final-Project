f2d7a6813c187f094ba7eb1ee5b028da696bc1ec
faizaanm <faizaan@berkeley.edu>
2021-05-08 12:02:21 -0700

    finished project files

--- contracts/MerchToken.sol
+++ contracts/MerchToken.sol
@@ -3,6 +3,35 @@ pragma solidity >=0.6.0 <0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+/**
+Accounts:
+(0) 0x110fbc9f146f3bc3427e3dc6d973ee5c32260ce1
+(1) 0x45917faf4623c28c65c130cf1e4020eb394560ba
+(2) 0x74a4b64575da1ea72c24241f5616a1400d78b154
+(3) 0x0d6398ab46a4db7a73a307dff7cb70af302bf645
+(4) 0x6184337a11ddbd29aebea04ae4ac048df4ae02c1
+(5) 0x1b306fd13ba28d1fec20d240a2536be94f8ac272
+(6) 0xe72c4ecc7db69c15c74840d1a103d95b6694b077
+(7) 0xbbfa39712c9116f6a6922175497f55a088642425
+(8) 0xe02b4bf22b5a46af9a08baf82f6243bf6861ff1e
+(9) 0xe3d746565f28df308b72f3f6eb7bdeb122d2505e
+
+Private Keys:
+(0) eed1a341a0ed90530a9dde2891921610125778861638c6533501acc4ffd83db9
+(1) 2c68d2c186a2f66b35a7fb7fc2a054404936346c2f671ffc8713ace257b3161b
+(2) 3ff9018c8b94f7395ce782840be0455aadae1a63c899a5027ba4d88a89cd19b4
+(3) e9848962bd6cdbb727f17e9f195930d682a9f430916fa6053f0c8a992efc0a29
+(4) 7396f7daca569468e6ee25dde63ba93ed7a63d542e132b0bb5cc61f3cdd4c2dc
+(5) ab1d5acad9f2efec8233cf3995dac54b0f25fbb72cb8069362a000b510d82b13
+(6) d2c9884aca9b76bde9406828c0c330d18e6dca877506882ab6d57efcd98a0cfb
+(7) 6f8ef6e7263a4797a4fb744643b90976415a1271b0f3e7eef7655ecd89013114
+(8) 22648f45757c341b717a8f81ba97cfa9a0be94e7123d086998dbb827f5d30818
+(9) dea278f104d2352a1ec4f4202883d3ffad0f50a1121071a3ec4893f76a921106
+
+Mnemonic: coil parent climb kick crash flag father wage radar electric shell guess
+ */
+
+
 contract MerchToken is ERC721, Ownable {
     /*** EVENTS ***/
     /// The event emitted (useable by web3) when a token is purchased
@@ -64,12 +93,12 @@ contract MerchToken is ERC721, Ownable {
 
         // ***********TO DO:*************  Generate psedorandom numbers
         // by running blocktimestamp+address -> blackbox hashing function -> determine from last digit of result
-        bytes32 hash1 = 
-        bytes32 hash2 = 
+        bytes32 hash1 = sha256(abi.encodePacked(block.number, msg.sender));
+        bytes32 hash2 = keccak256(abi.encode(hash1));
         uint256 result = uint256(hash2);
 
-        uint256 _merchType = 
-        uint256 _tag = 
+        uint256 _merchType = result % 10;
+        uint256 _tag = result % 5;
 
         require(_tag <= 4, "Failed random check, ID too large");
         require(_merchType <= 9, "Failed random check, tagID too large");
@@ -87,7 +116,7 @@ contract MerchToken is ERC721, Ownable {
         });
 
         //*****TO:DO****** once a token is minted, tell the contract by changing one variable
-        
+        index += 1;
 
         emit MerchToken(msg.sender, index);
     }
@@ -140,7 +169,9 @@ contract MerchToken is ERC721, Ownable {
     //******TO DO************
     // Changes the price that must be paid to buy a particular token
     function changePrice(uint256 _tokenId, uint256 _newPrice) public {
-
+        require(_newPrice != 0 && (_tokenId <= index));
+        require(ownerOf(_tokenId) == msg.sender);
+        tokenInfos[_tokenId].price = _newPrice;
     }
 
     // Allow a token to be sold
